d346c937617f802f47b99164672a020ea463cc4f
DuongGiaHoa <dghoa0603@gmail.com>
2018-09-13 11:49:36 +0700

    Create new project MathNoteApp

---
+++ MathNoteApp/app/src/main/java/com/example/capstone/mathnote_capstone/Activity/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.capstone.mathnote_capstone.Activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.capstone.mathnote_capstone.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView logo;
+    private static int splashTimeOut=5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo=(ImageView)findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, SplashActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
