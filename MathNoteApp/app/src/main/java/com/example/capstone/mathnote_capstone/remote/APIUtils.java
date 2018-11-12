c625de9235d42ab269951173714e4b823e226afb
Chip Caber <volamtkcuc@gmail.com>
2018-11-12 23:08:30 +0700

    Change API URL

--- MathNoteApp/app/src/main/java/com/example/capstone/mathnote_capstone/remote/APIUtils.java
+++ MathNoteApp/app/src/main/java/com/example/capstone/mathnote_capstone/remote/APIUtils.java
@@ -5,7 +5,7 @@ public class APIUtils {
     private APIUtils(){
     };
 
-    public static final String API_URL = "http://192.168.1.63:8080/capstone/android/";
+    public static final String API_URL = "http://mathformulas-env.svazsv2bhc.ap-southeast-1.elasticbeanstalk.com/android/";
 
     public static MathFormulasService getMathFormulasService() {
         return RetrofitClient.getClient(API_URL).create(MathFormulasService.class);
